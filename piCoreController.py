7032356c278f10e2d11a0b6a102220d41cf85cbf
Shaurya Tathgir <58607486+shauryaaaaaaaaaa@users.noreply.github.com>
2020-10-05 12:28:44 -0700

    Temporary standby mode added

--- piCoreController.py
+++ piCoreController.py
@@ -3,56 +3,54 @@ from squeezebox_controller import SqueezeBoxController
 from time import sleep
 from requests import get
 
-controller = SqueezeBoxController("192.168.86.39", 9000)
+controller = SqueezeBoxController(PLAYER_IP, PLAYER_PORT)
+
+listen = True
 
 def togglePlaying():
-    info = controller._get_player_info('HiPi Study')
+    info = controller._get_player_info(PLAYER_NAME)
     if(info['mode'] == 'pause'):
         execute("PLAY")
     else:
         execute("PAUSE")
 
-def volume(command):
-    if(command == 'up'):
-        execute("VOLUME UP")
-    elif(command == 'down'):
-        execute("VOLUME DOWN")
-    elif(command == 'mute'):
-        execute("MUTE")
-
 def execute(command):
     params = {
-      "player": "HiPi Study",
+      "player": PLAYER_NAME,
       "command": command
     }
     controller.simple_command(params)
     return
 
 def shutdown():
-    get('http://192.168.86.39/cgi-bin/main.cgi?ACTION=shutdown')
+    get('http://' + PLAYER_IP + '/cgi-bin/main.cgi?ACTION=shutdown')
     
 def on_press(key):
     try:
         key.char
     except AttributeError:
-        if(key == keyboard.Key.media_play_pause):
-            togglePlaying()
+        if(key == keyboard.Key.f9):
+            listen = !listen
             sleep(0.5)
-        elif(key == keyboard.Key.media_volume_up):
-            volume('up')
-            sleep(0.5)
-        elif(key == keyboard.Key.media_volume_down):
-            volume('down')
-            sleep(0.5)
-        elif(key == keyboard.Key.media_volume_mute):
-            volume('mute')
-            sleep(0.5)
-        elif(key == keyboard.Key.media_next):
-            execute("SKIP")
-            sleep(0.5)
-        elif(key == keyboard.Key.f10):
-            shutdown()
-            sleep(0.5)
+        elif(listen):
+            if(key == keyboard.Key.media_play_pause):
+                togglePlaying()
+                sleep(0.5)
+            elif(key == keyboard.Key.media_volume_up):
+                execute("VOLUME UP")
+                sleep(0.5)
+            elif(key == keyboard.Key.media_volume_down):
+                execute("VOLUME DOWN")
+                sleep(0.5)
+            elif(key == keyboard.Key.media_volume_mute):
+                execute("MUTE")
+                sleep(0.5)
+            elif(key == keyboard.Key.media_next):
+                execute("SKIP")
+                sleep(0.5)
+            elif(key == keyboard.Key.f10):
+                shutdown()
+                sleep(0.5)
         
 def on_release(key):
     pass
